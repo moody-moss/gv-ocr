d8fd4e316accdf8cdc7efab65904d1422b73dba8
moody-moss <athirahbajan6797@gmail.com>
2021-09-13 17:44:45 +0800

    efafsad

--- gvTest.js
+++ gvTest.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const cors = require('cors')
 const vision = require('@google-cloud/vision')
+const { json } = require('express')
 
 const app = express()
+
+app.use(express.json())
 
 app.use(cors({ origin: '*' }))
 app.options('*', cors()) // enable preflight across-the-board --> include bfeore other route
@@ -13,10 +16,25 @@ app.get('/coffee', (req, res) => {
     })
 })
 
-//C:/Users/nurathirah.bajan/Documents/AR Project/GV/fourth-buffer-325405-04aa91f83467.json
+// credential
 process.env.GOOGLE_APPLICATION_CREDENTIALS = './fourth-buffer-325405-04aa91f83467.json'
 
-async function gv_ocr() {
+async function x() {
+    const client = new vision.ImageAnnotatorClient()
+    const URL_Path = `https://qwe-1.herokuapp.com/out.jpeg`
+
+    const [result] = await client.textDetection(URL_Path)
+    const detections = result.textAnnotations
+
+    console.log('Text Detected: ')
+
+    detections.forEach(element =>
+        console.log(element)
+    );
+
+}
+
+function gv_ocr() {
 
     // Creates a client
     const client = new vision.ImageAnnotatorClient()
@@ -25,11 +43,12 @@ async function gv_ocr() {
     client.textDetection('https://qwe-1.herokuapp.com/out.jpeg')
         .then(results => {
             const result = results[0].textAnnotations
-            console.log(`Text Annotation Result: ${JSON.stringify(result[0], null, 2)}`)
+            console.log(`Text Annotation Result: ${JSON.stringify(result)}`)
+            
 
             app.get('/gv-txt', (req, res) => {
                 res.send(
-                    JSON.stringify(result[0])
+                    JSON.stringify(result)
                 )
             })
 
@@ -39,7 +58,34 @@ async function gv_ocr() {
         })
 }
 
+
+function gv_ocr_1() {
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient()
+
+    // Performs label detection on the image file -> https://qwe-1.herokuapp.com/out.jpeg
+    client.textDetection('https://qwe-1.herokuapp.com/out.jpeg')
+        .then(results => {
+            const result = results[0].textAnnotations
+            console.log(`Text Annotation Result: ${JSON.stringify(result[0].description)}`)
+
+            txtFromImg = JSON.stringify(result[0].description)
+
+            // app.get('/gv-txt-var', (req, res) => {
+            //     res.send({
+            //         GVtxt: txtFromImg
+            //     })
+            // })
+        })
+        .catch(err => {
+            console.log('Error:', err)
+        })
+}
+
+
 gv_ocr()
+
 
 const server = app.listen(process.env.PORT || 1000, () => {
     const port = server.address().port
