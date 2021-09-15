784f0f58d85b33d99a6c0dc8fec0830f3ba11bfa
moody-moss <athirahbajan6797@gmail.com>
2021-09-15 11:03:56 +0800

    f

--- gvTest.js
+++ gvTest.js
@@ -43,7 +43,7 @@ function gv_ocr() {
     client.textDetection('https://qwe-1.herokuapp.com/out.jpeg')
         .then(results => {
             const result = results[0].textAnnotations
-            const resultGet = JSON.stringify(result[0], null, 2)
+            const resultGet = JSON.stringify(result[0])
             console.log(`Text Annotation Result: ${resultGet}`)
             
 
